2fc97c484aabce179b806604f0125bf4d85ba0cd
Marcel Bezerra da Silva <gft.marcel.silva@br.experian.com>
2017-05-03 16:24:46 -0300

    admin routes

---
+++ module/AdminFactory/config/admin.routes.config.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'admin_routes' => [
+        'produto' => [
+           'nome' => 'Produtos',
+           'route' => '/admin/produtos/'
+        ],
+        'compra' => [
+            'nome' => 'Compras',
+            'route' => '/admin/compras/'
+        ],
+        'pagamento' => [
+            'nome' => 'Pagamentos',
+            'route' => '/admin/pagamentos/'
+        ],
+        'site' => [
+            'nome' => 'Site',
+            'route' => '/admin/site/'
+        ],
+        'usuario' => [
+            'nome' => 'Usuários',
+            'route' => '/admin/usuario/'
+        ]
+    ]
+];
