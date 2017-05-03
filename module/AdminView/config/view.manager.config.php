4572d9e8ff7db2aa792d27d135ee23e33c35b3b3
Marcel Bezerra da Silva <gft.marcel.silva@br.experian.com>
2017-05-03 15:47:57 -0300

    namespace view

--- module/AdminView/config/view.manager.config.php
+++ module/AdminView/config/view.manager.config.php
@@ -3,8 +3,8 @@
 return array(
     'view_manager' => array(
         'template_map' => array(
-            'admin/admin/index' => __DIR__ . '/../view/admin/index/index.twig',
-            'admin/admin/site' => __DIR__ . '/../view/admin/site/site.twig',
+            'admin/admin/index' => __DIR__ . '/../view/ecompassaro/admin/index/index.twig',
+            'admin/admin/site' => __DIR__ . '/../view/ecompassaro/admin/site/site.twig',
             ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
