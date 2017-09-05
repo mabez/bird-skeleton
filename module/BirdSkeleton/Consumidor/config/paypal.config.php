4789f2b02c75ba1dd2104c9e50b37980f8c12fa9
Marcel Bezerra da Silva <marcelbzrra@gmail.com>
2017-09-05 16:46:10 -0300

    Ajustando com regras de negocios do paypal

--- module/BirdSkeleton/Consumidor/config/paypal.config.php
+++ module/BirdSkeleton/Consumidor/config/paypal.config.php
@@ -1,9 +1,18 @@
 <?php
+
 return [
     'paypal' => [
-        'URL' => 'http://api-3t.sandbox.paypal.com',
-        'USER' => 'marcelbzrra-facilitator_api1.gmail.com',
-        'PWD' => 'LA4GMQEZWD5PRJCC',
-        'SIGNATURE' => 'AFcWxV21C7fd0v3bYYYRCpSSRl31Ale3KGWSOI5vV9Gt9KgwFz.vjL4x'
+      'sandbox'=>[
+        'client'=>[
+          'Id' => 'Ab0bF4XkjBN7JeU4SaH0XKhwZ5Nr0tLl_4-k2VSCcStJaw8JOYjy3NmU2255KGuloi2BvLBf3B94TdLQ',
+          'secret' => 'EF_C1JFWW6Vbhu7q7psq3iwASyjEgLX7nCanpPhYGTNvfatzKZhAu8tFdImEbyQDJRXFVY9Q4afZuPpT'
+        ]
+      ],
+      'live' => [
+        'client'=>[
+          'id' => 'AZUENtETNVRtBgK12JYhtsBXYYZ6KKcVd_1GczhMvn9VTtV2w6vux3INdHlOf4OtWi2R994quK7ahqt3',
+          'secret' => 'ECaBxmAsVHFaPlJYWqzgheJYWqaaPJvWsoJkAlx8N3vrUW7nXaa_-dvD9MOmElDW5qMm4Mr8qaDIj5Ba'
+        ]
+      ]
     ]
 ];
