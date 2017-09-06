5dd97a0c4870fd125f9c98c23b14bb022431932d
Marcel Bezerra da Silva <marcelbzrra@gmail.com>
2017-09-06 16:04:07 -0300

    eventos para pagamento pelo paypal

--- module/BirdSkeleton/Consumidor/config/paypal.config.php
+++ module/BirdSkeleton/Consumidor/config/paypal.config.php
@@ -3,12 +3,20 @@
 return [
     'paypal' => [
       'sandbox'=>[
+        'callback' => [
+          'cancel' => '',
+          'success' => ''
+        ],
         'client'=>[
           'Id' => 'Ab0bF4XkjBN7JeU4SaH0XKhwZ5Nr0tLl_4-k2VSCcStJaw8JOYjy3NmU2255KGuloi2BvLBf3B94TdLQ',
           'secret' => 'EF_C1JFWW6Vbhu7q7psq3iwASyjEgLX7nCanpPhYGTNvfatzKZhAu8tFdImEbyQDJRXFVY9Q4afZuPpT'
         ]
       ],
       'live' => [
+        'callback' => [
+          'cancel' => '',
+          'success' => ''
+        ],
         'client'=>[
           'id' => 'AZUENtETNVRtBgK12JYhtsBXYYZ6KKcVd_1GczhMvn9VTtV2w6vux3INdHlOf4OtWi2R994quK7ahqt3',
           'secret' => 'ECaBxmAsVHFaPlJYWqzgheJYWqaaPJvWsoJkAlx8N3vrUW7nXaa_-dvD9MOmElDW5qMm4Mr8qaDIj5Ba'
